161bcdf27e575c1488816472c1c1cdc3314856e6
Ralf Oliveira Ferreira <ralf.oliveira@inatel.br>
2022-02-22 17:12:30 -0300

    Authentication with Github and NextAuth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-	const isUserLoggedIn = true;
+	const session = useSession();
 
-	return isUserLoggedIn ? (
-		<button type="button" className={styles.signInButton}>
+	return session.status === "authenticated" ? (
+		<button
+			type="button"
+			className={styles.signInButton}
+			onClick={() => signOut()}
+		>
 			<FaGithub color="#04d361" />
-			Ralf Oliveira
+			{session.data.user.name}
 			<FiX color="#737380" className={styles.closeIcon} />
 		</button>
 	) : (
-		<button type="button" className={styles.signInButton}>
+		<button
+			type="button"
+			className={styles.signInButton}
+			onClick={() => signIn("github")}
+		>
 			<FaGithub color="#eba417" />
 			Sign in with Github
 		</button>
