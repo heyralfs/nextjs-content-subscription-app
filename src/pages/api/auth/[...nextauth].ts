512ba0aa598cd9425e0605ad3a57ff92608c764a
Ralf Oliveira Ferreira <ralf.oliveira@inatel.br>
2022-02-22 21:32:07 -0300

    Storing user in faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
 	// Configure one or more authentication providers
 	providers: [
@@ -10,4 +14,37 @@ export default NextAuth({
 		}),
 		// ...add more providers here
 	],
+
+	secret: process.env.SIGNING_KEY,
+
+	callbacks: {
+		async signIn({ user, account, profile, email, credentials }) {
+			try {
+				await fauna.query(
+					q.If(
+						q.Not(
+							q.Exists(
+								q.Match(
+									q.Index("user_by_email"),
+									q.Casefold(user.email)
+								)
+							)
+						),
+						q.Create(q.Collection("users"), { data: { email } }),
+						q.Get(
+							q.Match(
+								q.Index("user_by_email"),
+								q.Casefold(user.email)
+							)
+						)
+					)
+				);
+
+				return true;
+			} catch (err) {
+				console.log(err);
+				return false;
+			}
+		},
+	},
 });
